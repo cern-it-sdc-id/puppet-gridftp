81b099a1c33eec05b68feb8435f73e5e228b3f10
andrea-manzi <andrea.manzi@gmail.com>
2017-05-02 10:39:21 +0200

    fix epsv_ip default parameter

--- pkg/lcgdm-gridftp-0.2.3/manifests/install.pp
+++
@@ -1,8 +0,0 @@
-class gridftp::install (
-) inherits gridftp::params {
-
-    package {"globus-gridftp-server-progs":
-      ensure	=> present,
-      notify	=> [ Class["gridftp::config"], Class["gridftp::service"] ]
-    }
-}
--- pkg/lcgdm-gridftp-0.2.3/manifests/lemon.pp
+++
@@ -1,7 +0,0 @@
-class gridftp::lemon {
-
-  # gridftp lemon monitoring
-  lemon::metric{'34': }
-  lemon::metric{'30063': }
-
-}
